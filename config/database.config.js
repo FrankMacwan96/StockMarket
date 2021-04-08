93bc0d7fa2c02f7be9da7da64ed077daac888751
Frank Macwan <30623987+FrankMacwan96@users.noreply.github.com>
2021-04-08 00:49:42 -0400

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     //url: 'mongodb://localhost:27017/easy-notes'
-    url:  'mongodb+srv://testuser:testuser123@cluster0.mnj0f.mongodb.net/Registration?retryWrites=true&w=majority'
+    url:  'mongodb+srv://username:password@cluster0.mnj0f.mongodb.net/Registration?retryWrites=true&w=majority'
 }
